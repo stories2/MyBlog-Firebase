ce3e74ac3b6129ea3a2dd086070f1d2512601bd2
Kim HyunWoo <stories2@naver.com>
2018-07-27 17:19:01 +0900

    index : check client token is verified

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
 const functions = require('firebase-functions');
-
+var admin = require('firebase-admin');
+var adminManager = admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+
+exports.checkToken = functions.https.onRequest(function (request, response) {
+    userGeneratedToken = request.body["token"]
+    adminManager.auth().verifyIdToken(userGeneratedToken)
+        .then(function (decodedToken) {
+            responseMsg = {
+                "Result": "OK"
+            }
+            response.setHeader('Content-Type', 'application/json');
+            response.status(200).send(JSON.stringify(responseMsg))
+        })
+        .catch(function (error) {
+            console.log("error accured: " + JSON.stringify(error))
+            errorMsg = {
+                "Result": "Fail"
+            }
+            response.setHeader('Content-Type', 'application/json');
+            response.status(500).send(JSON.stringify(errorMsg))
+        })
+})
